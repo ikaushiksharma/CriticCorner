97f34322c870751565b84e2ea241b5bd5f2d1e24
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2023-02-19 11:50:50 +0530

    movie form validation in progress

--- frontend/src/components/admin/MovieForm.jsx
+++ frontend/src/components/admin/MovieForm.jsx
@@ -32,6 +32,29 @@ const defaultMovieInfo = {
   status: "",
 };
 
+const validateMovie = (movieInfo) => {
+  const { title, storyLine, language, releaseDate, status, type, genres, tags, cast } = movieInfo;
+  if (!title.trim()) return { error: "Title is missing!" };
+  if (!storyLine.trim()) return { error: "Story Line is missing!" };
+  if (!language.trim()) return { error: "language is missing!" };
+  if (!releaseDate.trim()) return { error: "Release Date is missing!" };
+  if (!status.trim()) return { error: "Status is missing!" };
+  if (!type.trim()) return { error: "Type is missing!" };
+  if (!genres.length) return { error: "Genres are missing" };
+  for (let gen of genres) {
+    if (!gen.trim()) return { error: "Invalid genres!" };
+  }
+  if (!tags.length) return { error: "Tags are missing" };
+  for (let tag of tags) {
+    if (!tag.trim()) return { error: "Invalid Tag!" };
+  }
+  if (!cast.length) return { error: "Cast and crew are missing" };
+  for (let c of cast) {
+    if (typeof c !== "object") return { error: "Invalid cast!" };
+  }
+  return { error: null };
+};
+
 export default function MovieForm() {
   const [movieInfo, setMovieInfo] = useState({ ...defaultMovieInfo });
   const [showWritersModal, setShowWritersModal] = useState(false);
@@ -40,6 +63,9 @@ export default function MovieForm() {
   const [selectedPosterForUI, setSelectedPosterForUI] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
+    const { error } = validateMovie(movieInfo);
+    if (error) return console.log(error);
+    console.log(movieInfo);
   };
   const { updateNotification } = useNotification();
   const updatePosterForUI = (file) => {
@@ -225,4 +251,3 @@ export default function MovieForm() {
     </>
   );
 }
-
